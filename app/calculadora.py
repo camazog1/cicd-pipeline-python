cb9fe7aa9488be05451fd7df256f4b1bb8b90483
MauricioCa07 <carrillomauricio007@gmail.com>
2026-04-17 19:56:19 -0500

    adding the tests

--- app/calculadora.py
+++ app/calculadora.py
@@ -2,14 +2,18 @@
 
 AUTOR = "jeronimo, Esteban, Carlos, Mauricio"
 
+
 def sumar(a, b):
     return a + b
+
 
 def restar(a, b):
     return a - b
 
+
 def multiplicar(a, b):
     return a * b
+
 
 def dividir(a, b):
     if b == 0:
