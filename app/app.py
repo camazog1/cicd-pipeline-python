2bc621465cafb975dd5dfe77923829d80df85cd7
esteban.muriel.roldan.contractor <esteban.muriel.roldan.contractor@bbva.com>
2026-04-18 08:36:12 -0500

    fixed sonar issue

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
-from .calculadora import sumar, restar, multiplicar, dividir
 from flask import Flask, render_template, request
+from .calculadora import sumar, restar, multiplicar, dividir
 
 # app/app.py
 
@@ -8,7 +8,27 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    """Principal function"""
+    """
+    Maneja la ruta principal de la aplicación web.
+
+    Soporta métodos GET y POST:
+    - GET: Renderiza la página inicial sin resultado.
+    - POST: Procesa los datos enviados desde el formulario para realizar
+      una operación matemática básica.
+
+    Parámetros esperados en el formulario (request.form):
+    - num1 (str): Primer número, convertible a float.
+    - num2 (str): Segundo número, convertible a float.
+    - operacion (str): Tipo de operación a realizar. Puede ser:
+        - "sumar"
+        - "restar"
+        - "multiplicar"
+        - "dividir"
+
+    Retorna:
+        Response: Renderiza la plantilla 'index.html' con el resultado de la operación
+        o un mensaje de error si ocurre alguna excepción.
+    """
     resultado = None
     if request.method == "POST":
         try:
--- app/calculadora.py
+++ app/calculadora.py
@@ -4,22 +4,30 @@ AUTOR = "jeronimo, Esteban, Carlos, Mauricio"
 
 
 def sumar(a, b):
-    """Suma dos números."""
+    """
+    funcion que recibe 2 numero y los suma.
+    """
     return a + b
 
 
 def restar(a, b):
-    """Resta dos números."""
+    """
+    funcion que recibe 2 numero y los resta.
+    """
     return a - b
 
 
 def multiplicar(a, b):
-    """Multiplica dos números."""
+    """
+    funcion que recibe 2 numero y los multiplica.
+    """
     return a * b
 
 
 def dividir(a, b):
-    """Divide dos números."""
+    """
+    funcion que recibe 2 numero y los divide.
+    """
     if b == 0:
         raise ZeroDivisionError("No se puede dividir por cero")
     return a / b
